600fae87b7d3ad1759ceb4a4096f40c50d4a25c5
Serge Keyser <sergekeyser@gmail.com>
2019-10-07 15:25:54 +0100

    made changes for intermediate authorization

--- tmfDemo/src/app/login/login.page.ts
+++ tmfDemo/src/app/login/login.page.ts
@@ -3,6 +3,10 @@ import {  MenuController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Router } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
+import OktaAuth from '@okta/okta-auth-js';
+import { HttpModule } from '@angular/http';
+
 
 @Component({
   selector: 'app-login',
@@ -24,14 +28,16 @@ export class LoginPage implements OnInit {
 		console.log(this.password);
 		const options = {
 			headers: {
-					'Authorization': '01234567BBC'
-				}
+					//'Authorization': 'Bearer eyJraWQiOiJHMVNLWGhrNV9KVWNfZUVfYWt3VXFDT3FIblltby04MGxPZ3FocWR2TWFrIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULnJ6VjBaeENMTXJrRF80c01tUjkyMTBuMmtIRllHbGFIWEJEbG9pOUhKR3ciLCJpc3MiOiJodHRwczovL2Rldi02MDIxNDIub2t0YS5jb20vb2F1dGgyL2RlZmF1bHQiLCJhdWQiOiJhcGk6Ly9kZWZhdWx0IiwiaWF0IjoxNTcwNDU3NjM0LCJleHAiOjE1NzA0NjEyMzQsImNpZCI6IjBvYTFpOTN5cWtvYnN0azNoMzU3IiwidWlkIjoiMDB1MWk5MG00MXJLODlkM3QzNTciLCJzY3AiOlsib3BlbmlkIl0sInN1YiI6ImpvaG55bWFuQG1haWxpbmF0b3IuY29tIn0.h6heTca4lIQ7_RQ1jEh7fw1e5Up-kP8m8AJ7ch0L4jBJdnPtLAacsgzITB1ZCk4YZ_jelXqzjx53D1srEqz-AmU01zZkW5zrp1_YD8vEeVWYk0DC-bFruA-AvkuMHzcw-34gNxutwtiZFcUfRJAbR3w-KYpyiRec9_RRceaiL91h699oJXPQKMTlzJYCGN0cKrEyui1_RxzrolPAZUYS9Ra6hm-HFxPkAQqi5uIBfzp6PFfdBSk37cHx0isqWx8WVz8_Lb2dL73xRHo5997jglWmhF3yx5mgb-XdWguhqdA56ZJhb9TJ909BXP3EKsFh2BLkiwYLbfpM26YiyafZCQ'
+					'Authorization': this.name
+					
+			}
 		}
 		
 		this.customerdata = this.httpClient.get('http://e-digital-telco-mule-app.uk-e1.cloudhub.io/api/federated/userinfo',options);
 		this.customerdata 
 		.subscribe(data => {
-		   if (data.given_name == this.name) {
+		   if (data.email == this.name) {
 			  var navigationExtras = {
 				  queryParams: {
 					jsondata: JSON.stringify(data)
